5f0deb3e025ade239ddeeb8e42042a3eff3bdb10
barnytan <barnytan@gmail.com>
2015-09-24 23:07:53 +0800

    Created empty Storage class

---
+++ src/Storage.java
@@ -0,0 +1,32 @@
+
+public class Storage {
+
+	public static void readFile(){
+		// TODO
+	}
+	
+	public static void saveFile(){
+		// TODO
+	}
+	
+	public static void search(){
+		// TODO
+	}
+	
+	public static void add(){
+		// TODO
+	}
+	
+	public static void delete(){
+		// TODO
+	}
+	
+	public static void edit(){
+		// TODO
+	}
+	
+	public static void acknowledge(){
+		// TODO
+	}
+
+}
