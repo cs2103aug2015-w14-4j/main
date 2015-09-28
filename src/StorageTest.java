5b343959ef9493b3b488aa18a71110a26454aa69
barnytan <barnytan@gmail.com>
2015-09-28 10:02:19 +0800

    Added test case for Saving and Reading to/from file

--- src/StorageTest.java
+++ src/StorageTest.java
@@ -25,4 +25,19 @@ public class StorageTest {
 	public void testDelete(){
 		assertEquals("test",Storage.delete(0));
 	}
+	
+	@Test
+	public void testSave() throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm");
+		String dateInString = "28-09-2015 08:00";
+		Date date = sdf.parse(dateInString);
+		String name = "test2";
+		Storage.add(name, date);
+		assertEquals(true,Storage.saveFile());
+	}
+	
+	@Test
+	public void testRead(){
+		Storage.readFile();
+	}
 }
