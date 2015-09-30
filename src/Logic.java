ce028042d67da23c3b9a192b94008fc250c993f7
Donald Shum <donald_shum@hotmail.com>
2015-09-30 13:29:19 +0800

    Creating attributes, constructor and methods for Logic.java
    creating methods to be called by GUI and also calling parser to break
    down instructions.

--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,43 @@
 
 class Logic{
+	//Attributes
+	Storage store;
+	Parser parser;
+	String command;
+	String content;
+	Date date;
 	
+	//Constructor
+	public void Logic(){
+		store = new Storage();
+		parser = new Parser();
+	}
+	
+	//Methods
+	
+	public void executeCMD(String s){
+		parser.parse(s);
+		command = parser.getCommand();
+		content = parser.getContent();
+	}
+	
+	public void add(){
+		
+	}
+	
+	public void edit(){
+		
+	}
+	
+	public void delete(){
+		
+	}
+	
+	public void search(){
+		
+	}
+	
+	public void acknowledge(){
+		
+	}
 }
