c6b97fe370058380d4f66e54b80f170c58b366ce
barnytan <barnytan@gmail.com>
2015-10-03 01:42:50 +0800

    Update Daykeeper

--- SpeedyGUI/src/application/Daykeeper.java
+++ SpeedyGUI/src/application/Daykeeper.java
@@ -1,19 +1,34 @@
 package application;
+
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Daykeeper {
+	private final static String DAY_OF_THE_WEEK = "EEEE";
+	private final static String TODAYS_DATE = "dd MMMM yyyy";
 
 	public static String todayDay(){
-		DateFormat dateFormat = new SimpleDateFormat("EEEE");
+		DateFormat dateFormat = new SimpleDateFormat(DAY_OF_THE_WEEK);
 		Date date = new Date();
 		return dateFormat.format(date);
 	}
 	
 	public static String todayDate(){
-		DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+		DateFormat dateFormat = new SimpleDateFormat(TODAYS_DATE);
 		Date date = new Date();
 		return dateFormat.format(date);
+	}
+	
+	public static Date stringToDate(String dateText){
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm");
+		try {
+			return sdf.parse(dateText);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
