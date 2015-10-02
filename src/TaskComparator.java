8dd48d3bbc9f38c81f470557c03a952cec1c1ffd
barnytan <barnytan@gmail.com>
2015-10-02 19:19:08 +0800

    Created TaskComparator to sort tasks in order of earliest date

---
+++ src/TaskComparator.java
@@ -0,0 +1,22 @@
+import java.util.Comparator;
+
+public class TaskComparator implements Comparator<Task> {
+
+	public static final int SAME_DATE = 0;
+
+	@Override
+	public int compare(Task first, Task second) {
+		// TODO Auto-generated method stub
+		int result = SAME_DATE;
+		
+		if (first.getStartDate() != null && second.getStartDate() != null) {
+			result = first.getStartDate().compareTo(second.getStartDate());
+		}
+		if (result == SAME_DATE && first.getEndDate() != null && second.getEndDate() != null) {
+			return first.getEndDate().compareTo(second.getEndDate());
+		} else {
+			return result;
+		}
+	}
+
+}
