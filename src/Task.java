05e72a4964902f5685b613c1218722b9cb791413
barnytan <barnytan@gmail.com>
2015-10-02 17:47:56 +0800

    Updated Task object to support javaFx

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,8 @@
 import java.util.Date;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 // Basic Task Object
 public class Task {
  private String name;
@@ -20,6 +24,14 @@ public class Task {
 	 this.setStartDate(date);
 	 this.setEndDate(null);
 	 this.setDetails(null);
+	 this.setAcknowledged(false);
+ }
+ 
+ public Task(String name, String details){
+	 this.setName(name);
+	 this.setStartDate(null);
+	 this.setEndDate(null);
+	 this.setDetails(details);
 	 this.setAcknowledged(false);
  }
  
@@ -84,13 +96,26 @@ public String toString() {
    return "Task [name=" + name + ", details=" + details + ", startDate="
 	+ startDate + ", endDate=" + endDate + ", acknowledged=" 
 		   + isAcknowledged +"]";
-} 
+}
 
+/**
+ * Method to check if the Task Object contains a search term.
+ * 
+ * @return True if the search term exists
+ */
 public boolean contains(String searchTerm){
 	if(name.contains(searchTerm) || details.contains(searchTerm)){
 		return true;
 	}
 	return false;
+}
+
+public StringProperty getNameProperty(){
+	return new SimpleStringProperty(name);
+}
+
+public StringProperty getDetailsProperty(){
+	return new SimpleStringProperty(details);
 }
 
 }
