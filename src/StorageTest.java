4ef9e098061315a652c8afeb17b5aa14e360956d
barnytan <barnytan@gmail.com>
2015-10-02 17:47:23 +0800

    Updated StorageTest to work with non static methods

--- src/StorageTest.java
+++ src/StorageTest.java
@@ -7,37 +7,34 @@ import java.util.Date;
 import org.junit.Test;
 
 public class StorageTest {
-
-	public void test() {
-		fail("Not yet implemented"); // TODO
-	}
+	
 
 	@Test
 	public void testAdd() throws ParseException{
+		Storage store = new Storage();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm");
 		String dateInString = "27-09-2015 08:00";
 		Date date = sdf.parse(dateInString);
 		String name = "test";
-		assertEquals(true,Storage.add(name, date));
+		assertEquals(true,store.add(name, date));
+		store.saveFile();
 	}
 	
 	@Test
 	public void testDelete(){
-		assertEquals("test",Storage.delete(0));
+		Storage store = new Storage();
+		assertEquals("test",store.delete(0));
+		store.saveFile();
 	}
 	
 	@Test
 	public void testSave() throws ParseException{
+		Storage store = new Storage();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm");
 		String dateInString = "28-09-2015 08:00";
 		Date date = sdf.parse(dateInString);
 		String name = "test2";
-		Storage.add(name, date);
-		assertEquals(true,Storage.saveFile());
-	}
-	
-	@Test
-	public void testRead(){
-		Storage.readFile();
+		store.add(name, date);
+		assertEquals(true,store.saveFile());
 	}
 }
