133a79cbcb44c96684575c5fe62547bf70aa7bf4
jianghc93 <jianghc93@hotmail.com>
2015-10-07 01:39:42 +0800

    Changed Hashtable in RootController to use taskId as key and TaskController as object

--- src/controller/RootController.java
+++ src/controller/RootController.java
@@ -3,6 +3,7 @@ package controller;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Hashtable;
+import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,8 +33,8 @@ public class RootController {
 	
 	// Reference to the main application.
     private MainApp mainApp;
-    // Maps the Task to its corresponding TitledPane
-    private Hashtable<Task, TitledPane> taskLookupTable;
+    // Maps the Task to its corresponding TaskController
+    private Hashtable<Integer, TaskController> taskLookupTable;
     private Logic logic;
 	
     /**
@@ -44,7 +45,7 @@ public class RootController {
     private void initialize() {
     	// Initialize the tasks and fetch a list of tasks
     	logic = new Logic();
-    	taskLookupTable = new Hashtable<Task, TitledPane>();
+    	taskLookupTable = new Hashtable<Integer, TaskController>();
     	initTasksList();
     	userName.setText("Hi Joe");
     	todayDay.setText(DayProcessor.todayDay());
@@ -60,6 +61,7 @@ public class RootController {
         this.mainApp = mainApp;
     }
     
+    //TODO Could move this method to TaskController
     private TitledPane createTitledPane(Task t){
     	FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/view/TaskView.fxml"));
@@ -73,7 +75,8 @@ public class RootController {
         TaskController tc = loader.getController();
         tc.setName(t.getName());
         tc.setDetails(t.getDetails());
-        taskLookupTable.put(t, tp);
+        tc.setTaskId(t.getTaskId());
+        taskLookupTable.put(t.getTaskId(), tc);
     	return tp;
     }
     
@@ -81,13 +84,23 @@ public class RootController {
     	containerOfTasks.getPanes().add(createTitledPane(t));
     }
     
-    private void removeTasks (int index){
-    	containerOfTasks.getPanes().remove(index);
+    private void removeTask(int taskID){
+    	TaskController tc = taskLookupTable.get(taskID);
+    	//System.out.println("removeTask taskID: " + taskID);
+    	//System.out.println("removeTask method removed: " + tc.getContainer().getText());
+    	containerOfTasks.getPanes().remove(tc.getContainer());
+    	taskLookupTable.remove(taskID);
     }
     
     private void initTasksList() {
     	for (int i = 0; i < logic.getNumOfTask(); i++) {
     		addTask(logic.getTask(i));
+		}
+    }
+    
+    private void loadTasksList(List<Task> listOfTasks) {
+    	for (int i = 0; i < listOfTasks.size(); i++) {
+    		addTask(listOfTasks.get(i));
 		}
     }
             
@@ -96,13 +109,29 @@ public class RootController {
         // Command was entered, do something...
     	String userInput = commandBox.getText();
         System.out.println(userInput);
-        GuiCommand gC = logic.executeCMD(userInput);
+        //GuiCommand gc = logic.executeCMD(userInput);
+        GuiCommand gc = new GuiCommand(COMMANDS.DELETE, "Delete", logic.getTask(0));
+        System.out.println(logic.getTask(0).getTaskId());
+        System.out.println(logic.getTask(3).getTaskId());
+        //System.out.println("Trying to delete: " + logic.getTask(0).getName());
+        COMMANDS command = gc.getCmd();
         try{
-        //addTask(t);
+        	switch(command){
+        		case ADD: {
+        			addTask(gc.getTask());
+        			// TODO set the feedback
+        			break;
+        		}
+        		case DELETE: {
+        			removeTask(gc.getTaskId());
+        			// TODO set the feedback
+        			break;
+        		}
+    		}
         } catch (Exception e){
         	ErrorProcessor.alert(e.toString());
         }
         commandBox.clear();
     }
-    
+        
 }
