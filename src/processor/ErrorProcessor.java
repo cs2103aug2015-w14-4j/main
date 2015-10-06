adfdfc155cc03289ca53acb133c5b67a630455fc
barnytan <barnytan@gmail.com>
2015-10-06 09:03:58 +0800

    ErrorProcessor now supports testing without javafx

--- src/processor/ErrorProcessor.java
+++ src/processor/ErrorProcessor.java
@@ -4,24 +4,33 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class ErrorProcessor {
-	
+
 	private static final String ERROR_TITLE = "Error";
 	private static final String ERROR_TRIGGERED = "Error Triggered";
 	private static final String ERROR_TRIGGERED_AT = "Error Triggered at ";
-	
-	public static void alert(String error){
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle(ERROR_TITLE);
-		alert.setHeaderText(ERROR_TRIGGERED);
-		alert.setContentText(error);
-		alert.showAndWait();
+
+	public static void alert(String error) {
+		try {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle(ERROR_TITLE);
+			alert.setHeaderText(ERROR_TRIGGERED);
+			alert.setContentText(error);
+			alert.showAndWait();
+		} catch (java.lang.ExceptionInInitializerError e) {
+			System.out.println(error);
+		}
 	}
-	
-	public static void alert(String errorLocation, String error){
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle(ERROR_TITLE);
-		alert.setHeaderText(ERROR_TRIGGERED_AT + errorLocation);
-		alert.setContentText(error);
-		alert.showAndWait();
+
+	public static void alert(String errorLocation, String error) {
+		try {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle(ERROR_TITLE);
+			alert.setHeaderText(ERROR_TRIGGERED_AT + errorLocation);
+			alert.setContentText(error);
+			alert.showAndWait();
+		} catch (java.lang.ExceptionInInitializerError e) {
+			System.out.println(ERROR_TRIGGERED_AT + errorLocation);
+			System.out.println(error);
+		}
 	}
 }
