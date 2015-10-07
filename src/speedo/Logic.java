05916a495d881b85e8e91e7ba51b2cb5c7590a2b
barnytan <barnytan@gmail.com>
2015-10-07 09:14:06 +0800

    Reverted Donald's code in Logic

--- src/speedo/Logic.java
+++ src/speedo/Logic.java
@@ -1,4 +1,5 @@
 package speedo;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,95 +9,106 @@ import processor.COMMANDS;
 import processor.COMMANDS;
 import storage.Storage;
 
-
-public class Logic{
-	//Attributes
+public class Logic {
+	// Attributes
 	private Storage store;
 	private Parser parser;
 	private COMMANDS command;
 	private String content;
 	private int taskIndex;
 
-	
-	//Constructor
-	public Logic(){
+	// Constructor
+	public Logic() {
 		store = new Storage();
 		parser = new Parser();
 	}
-		
-	//Methods
-	public GuiCommand executeCMD(String s) throws ParseException{
+
+	// Methods
+	public GuiCommand executeCMD(String s) throws ParseException {
 		parser.parse(s);
 		command = parser.getCommand();
 		content = parser.getContent();
 		GuiCommand c = null;
-		
-		switch(command){
-		
-		case ADD: Task t = add(); c = new GuiCommand(COMMANDS.ADD, "Added " + t.getName(), t); break;
-		case DELETE: int taskId = delete(); c =  new GuiCommand(COMMANDS.DELETE, "Deleted the task", taskId); break;
-		case EDIT: Task t = edit(, s); c = new GuiCommand(COMMANDS.EDIT, "Edited", t); break;
-		case SEARCH: ; break;
-		case ACK: ; break;
-		case INVALID: ; break;
+		Task t = null;
+		List<Task> list = null;
+
+		switch (command) {
+
+		case ADD:
+			t = add();
+			c = new GuiCommand(COMMANDS.ADD, "Added " + t.getName(), t);
+			break;
+		case DELETE:
+			int taskId = delete();
+			c = new GuiCommand(COMMANDS.DELETE, "Deleted the task", taskId);
+			break;
+		case EDIT:
+			taskIndex = parser.getIndex();
+			t = edit(taskIndex, s);
+			c = new GuiCommand(COMMANDS.EDIT, "Edited", t);
+			break;
+		case SEARCH:
+			list = search();
+			c = new GuiCommand(COMMANDS.SEARCH, "Searched tasks", list);
+			break;
+		case ACK:
+			taskIndex = parser.getIndex();
+			t = acknowledge(taskIndex);
+			c = new GuiCommand(COMMANDS.ACK, "Acknowledged", t);
+		case INVALID:
+			c = new GuiCommand(COMMANDS.INVALID, "Invalid command");
+			break;
 		}
 		return c;
 	}
-	
-	public Task getTask(int index){
+
+	public Task getTask(int index) {
 		return store.getTask(index);
 	}
-	
-	public int getNumOfTask(){
+
+	public int getNumOfTask() {
 		return store.getNumOfTask();
 	}
-	
-	public Task add() throws ParseException{
+
+	public Task add() throws ParseException {
 		/*
-		Date date = null;
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm");
-		String date_in_string = sdf.format(new Date()); 
-		try {
-			 date = sdf.parse(date_in_string);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
+		 * Date date = null; SimpleDateFormat sdf = new SimpleDateFormat(
+		 * "dd-M-yyyy hh:mm"); String date_in_string = sdf.format(new Date());
+		 * try { date = sdf.parse(date_in_string); } catch (ParseException e) {
+		 * // TODO Auto-generated catch block e.printStackTrace(); }
+		 */
 		Date date = parser.getDate();
 		Task newTask = store.add(content, date);
-		if(newTask != null){
+		if (newTask != null) {
 			System.out.println("Task added");
-			//store.saveFile();
+			// store.saveFile();
 			return newTask;
 		} else {
 			return null;
 		}
 	}
-	
-	public void edit(int index, String text){
-		store.edit(index, text);
-		//store.saveFile();
+
+	public Task edit(int index, String text){
+		return store.edit(index, text);
+		// store.saveFile();
 	}
-	
-	public void delete(){
+
+	public int delete(){
 		int index = Integer.parseInt(content);
 		System.out.println("Task deleted");
-		store.delete(index);
-		//store.saveFile();
+		return store.delete(index);
+		// store.saveFile();
 	}
-	
-	public void search(){
+
+	public List<Task> search(){
 		List<Task> list = store.search(content);
-		while (!list.isEmpty()){
-			System.out.println(list.get(0));
-			list.remove(0);
-		}
+		return list;
+
 	}
-	
-	public void acknowledge(){
-		store.acknowledge(Integer.parseInt(content));
-		//store.saveFile();
+
+	public Task acknowledge(int taskIndex){
+		return store.acknowledge(taskIndex);
+		// store.saveFile();
 	}
-	
+
 }
