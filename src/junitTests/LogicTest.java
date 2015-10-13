e7c8f4a6c93caa30e767a67a253471374bf3c21c
Donald Shum <donald_shum@hotmail.com>
2015-10-13 20:19:59 +0800

    Added jUnit test cases to logic

--- src/junitTests/LogicTest.java
+++ src/junitTests/LogicTest.java
@@ -1,5 +1,23 @@
 package junitTests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class LogicTest {
 
+	@Test
+	public void testAdd() {
+
+	}
+	
+	@Test
+	public void testDelete() {
+
+	}
+		
+	@Test
+	public void testExecuteCmd() {
+
+	}
 }
