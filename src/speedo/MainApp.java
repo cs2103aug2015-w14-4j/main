5199b01892463463ec18d4f97cb8ba9c510db12f
jianghc93 <jianghc93@hotmail.com>
2015-10-17 10:57:14 +0800

    MainApp now handles GUI logic and loads RootView

--- src/speedo/MainApp.java
+++ src/speedo/MainApp.java
@@ -1,24 +1,33 @@
 package speedo;
 
-import controller.RootController;
+import java.io.IOException;
+
+import controller.CommandBoxController;
+import controller.TaskListController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-
 public class MainApp extends Application {
     
+	private static final String FXML_PATH = "/view/RootView.fxml";
+
     private Stage primaryStage;
     private BorderPane rootLayout;
+    private Logic logic;
         
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
         this.primaryStage.setTitle("SpeeDo");
-
         initRootLayout();
+        Scene scene = new Scene(rootLayout,850,500);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		System.out.println(rootLayout.getBottom().getScene().widthProperty());
 	}
 	
 	public static void main(String[] args) {
@@ -26,32 +35,62 @@ public class MainApp extends Application {
 	}
     
     /**
-     * Constructor
-     */
-    public MainApp() {
-    	
-    }
-    
-    /**
      * Initializes the root layout.
      */
     public void initRootLayout() {
+    	FXMLLoader loader = new FXMLLoader();         
+        loader.setLocation(getClass().getResource(FXML_PATH));
 		try {
-			FXMLLoader loader = new FXMLLoader();         
-	        loader.setLocation(getClass().getResource("/view/RootView.fxml"));
-	        
-	        rootLayout = (BorderPane) loader.load();
-	        // need to load before getting controller
-	        RootController controller = loader.getController();
-	        controller.setMainApp(this);
-			Scene scene = new Scene(rootLayout,850,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			rootLayout = (BorderPane) loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+    	logic = new Logic();
+		
+		// sets up the list of tasks to display
+		TaskListController tlc = new TaskListController();
+    	tlc.loadTaskList(logic.getTaskList());
+    	rootLayout.setRight(tlc);
+    	
+    	// sets up the command box
+    	CommandBoxController cbc = new CommandBoxController();
+    	cbc.setMainApp(this);
+    	rootLayout.setBottom(cbc);
+    	
+    	//userName.setText("Hi Jim");
+    	//todayDay.setText(DayProcessor.todayDay());
+    	//todayDate.setText(DayProcessor.todayDate());
     }
     
+    public void handleUserCommand(String userInput) {
+        // Command was entered, do something...
+        System.out.println(userInput);
+        GuiCommand command = logic.executeCMD(userInput);
+        TaskListController tlc = (TaskListController) rootLayout.getRight(); 
+        CommandBoxController cbc = (CommandBoxController) rootLayout.getBottom(); 
+        
+        switch(command.getCmd()){
+	        case ADD: {
+	        	tlc.addTask(command.getTask());
+	        	cbc.setFeedback(command.getMsg());
+	        	break;
+	        }
+	        case DELETE: {
+	        	tlc.removeTask(command.getTask());
+	        	cbc.setFeedback(command.getMsg());
+	        	break;
+	        }
+	        case EDIT: {
+	        	tlc.editTask(command.getTask());
+	        	cbc.setFeedback(command.getMsg());
+	        	break;
+	        }
+	        default:
+	        	cbc.setFeedback("Invalid command");
+			break;
+        }
+    }
     
 }
