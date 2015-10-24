df500eb0beb6a74993483b5d88e3d64251c9007e
Donald Shum <donald_shum@hotmail.com>
2015-10-24 11:45:07 +0800

    Updating jUnit test case

--- src/junitTests/LogicTest.java
+++ src/junitTests/LogicTest.java
@@ -2,22 +2,53 @@ package junitTests;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
-public class LogicTest {
+import processor.COMMANDS;
+import speedo.GuiCommand;
+import speedo.Logic;
+import storage.Task;
 
+public class LogicTest {
+	Logic l = new Logic(true);
+	String input = "add \"task1\" 20102015 1000 'detail'";
+	static String details = "detail";
+	static String name = "task1";
+	static SimpleDateFormat dateVariant4 = new SimpleDateFormat("ddMMyyyy hhmm");
+	static Date date;
+	static Task testT;
+	static 	GuiCommand testGCommand;
+	
+	/*
 	@Test
 	public void testAdd() {
-
+		
 	}
 	
 	@Test
 	public void testDelete() {
 
 	}
+	*/
 		
 	@Test
 	public void testExecuteCmd() {
-
-	}
+		try{
+			date = dateVariant4.parse(" 20102015 1000");
+		} catch (Exception e){
+				
+		}
+		testT = new Task(name, details, date);
+		testGCommand = new GuiCommand(COMMANDS.ADD, "Added task1", testT);
+		
+		GuiCommand get = l.executeCMD(input);
+		System.out.println(get.getMsg() + " " + get.getTask());
+		System.out.println("TestT: " + testT);
+		 assertEquals(testT.getName(), get.getTask().getName());
+		 assertEquals(testT.getDetails(), get.getTask().getDetails());
+		 assertEquals(testT.getStartDate(), get.getTask().getStartDate());
+		}
 }
