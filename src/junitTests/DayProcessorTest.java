485265cbe5daf75976fce566cca5d962cd163d68
barnytan <barnytan@gmail.com>
2015-11-04 02:40:32 +0800

    Updated DayProcessorTest

--- src/junitTests/DayProcessorTest.java
+++ src/junitTests/DayProcessorTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -12,20 +13,25 @@ import processor.DayProcessor;
 
 public class DayProcessorTest {
 
+	private static final String SPACE = " ";
 	@Test
 	public void testStringToDate() {
-		DatePair gg = DayProcessor.stringToDate(" 12 nov 2015       0800");
-		System.out.println(gg.getDateOne()+" "+gg.getDateTwo());
-		//System.out.println(DayProcessor.stringToDate(""));
-		String a = "12:/ oc-t /no -d v: 2015";
-		System.out.println(a.replaceAll("[:/-]", " "));
-		System.out.println(a.split(" -d ").length);
-		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy hhmm");
+		String dateOne = "12 november 2015";
+		String dateTwo = "13 11 2015";
+		DatePair datePair = DayProcessor.stringToDate(dateOne+SPACE+dateTwo);
+
+		Date expectedOne = new Date();
+		Date expectedTwo = new Date();
+		SimpleDateFormat expOneFormat = new SimpleDateFormat("dd MMM yyyy");
+		SimpleDateFormat expTwoFormat = new SimpleDateFormat("dd MM yyyy");
 		try {
-			System.out.println(sdf.parse("121115 0800"));
+			expectedOne = expOneFormat.parse(dateOne);
+			expectedTwo = expTwoFormat.parse(dateTwo);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 		}
+		assertEquals(datePair.getDateOne(), expectedOne);
+		assertEquals(datePair.getDateTwo(), expectedTwo);
 	}
 
 }
