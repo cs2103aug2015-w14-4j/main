44f57d43360fdbe88e7258e2dffcddfef8728a38
jianghc93 <jianghc93@hotmail.com>
2015-11-07 11:00:35 +0800

    Empty fields in the expanded view now removed

--- src/controller/TaskController.java
+++ src/controller/TaskController.java
@@ -1,11 +1,11 @@
 package controller;
 
 import java.io.IOException;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import processor.ErrorProcessor;
 import storage.Task;
@@ -13,69 +13,98 @@ import storage.Task;
 public class TaskController extends TitledPane{
 	
 	private static final String FXML_PATH = "/view/TaskView.fxml";
+	private static final String TASK_NAME_DISPLAY = "%d. %s";
 	
 	@FXML 
-	private Text details;
+	private VBox expandedBox;
+	
 	@FXML 
-	private Label dateLabel;
+	private HBox details;
 	@FXML 
-	private Text startDate;
+	private HBox endDate;
 	@FXML 
-	private Text endDate;
+	private HBox startDate;
+	
+	@FXML 
+	private Text detailsText;
+	@FXML 
+	private Text startDateText;
+	@FXML 
+	private Text endDateText;
 	
 	private int taskId;
 	private int taskIndex;
 	
-	public TaskController(Task t, int index){
-    	FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource(FXML_PATH));
-        loader.setRoot(this);
-        loader.setController(this);
+	public TaskController(Task task, int index){
         try {
+        	FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource(FXML_PATH));
+            loader.setRoot(this);
+            loader.setController(this);
         	loader.load();
-	        setTaskIndex(index);
-        	setName(t.getName());
-	        setStartDate(t.getStartDateString() + "   " + t.getStartTimeString());
-	        setEndDate(t.getEndDateString() + "   " + t.getEndTimeString());
-	        setDetails(t.getDetails());
-	        setTaskId(t.getTaskId());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			ErrorProcessor.alert(e.toString());
 		}
+        initTask(task, index);
+	}
+	
+	@FXML
+    public void initialize() {
         this.setExpanded(false);
-        if(t.isCompleted()){
+    }
+	
+	private void initTask(Task task, int index){
+        setTaskId(task.getTaskId());
+    	setTaskIndex(index);
+		setName(task.getName());
+        setDetails(task.getDetails());
+        setStartDate(task.getStartDateString());
+        setEndDate(task.getEndDateString()); 
+        applyStyling(task);
+	}
+	
+	private void applyStyling(Task task){
+        if(task.isCompleted()){
         	this.setId("taskCompleted");
         	this.setOpacity(0.2);
-        } else if(t.due() == Task.OVERDUE){
+        } else if(task.due() == Task.OVERDUE){
         	this.setId("taskOverdue");
-        } else if(t.due() == Task.TODAY){
+        } else if(task.due() == Task.TODAY){
         	this.setId("taskDueToday");
-        } else if(t.due() == Task.TOMORROW){
+        } else if(task.due() == Task.TOMORROW){
         	this.setId("taskDueTomorrow");
-        } else if(t.due() == Task.NODATE){
+        } else if(task.due() == Task.NODATE && task.getDetails() == null){
         	this.setCollapsible(false);
         }
 	}
 	
 	public void setName(String name){
-		this.setText(taskIndex + ". " + name);
+		this.setText(String.format(TASK_NAME_DISPLAY, taskIndex, name));
 	}
 	
-	public void setDetails(String d){
-		this.details.setText(d);
+	public void setDetails(String text){
+		if(text == null){
+			expandedBox.getChildren().remove(details);
+		} else {
+			detailsText.setText(text);
+		}
 	}
 	
-	public void setStartDate(String d){
-		this.startDate.setText(d);
+	public void setStartDate(String date){
+		if(date == null){
+			expandedBox.getChildren().remove(startDate);
+		} else {
+			startDateText.setText(date);
+		}
 	}
 	
 	public void setEndDate(String d){
-		this.endDate.setText(d);
+		endDateText.setText(d);
 	}
 	
 	public void setTaskId(int id){
-		this.taskId = id;
+		taskId = id;
 	}
 	
 	public int getTaskId(){
@@ -83,7 +112,7 @@ public class TaskController extends TitledPane{
 	}
 	
 	public void setTaskIndex(int index){
-		this.taskIndex = index;
+		taskIndex = index;
 	}
 	
 }
