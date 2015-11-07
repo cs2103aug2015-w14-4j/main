58fde0a5fe6678c46dc5c13c5e95943a2f83a29b
barnytan <barnytan@gmail.com>
2015-11-07 22:54:19 +0800

    Refactoring

--- src/processor/COMMANDS.java
+++ src/processor/COMMANDS.java
@@ -1,5 +1,13 @@
+//@@author A0125369Y
 package processor;
 
+/**
+ * COMMANDS is a class file that contain the command types.
+ * <p>
+ * This class is to ensure that all components standardize to the same set of
+ * commands.
+ *
+ */
 public enum COMMANDS {
 	ADD, DELETE, EDIT, ACK, SEARCH, INVALID, EXIT, HOME, UNDO, EXPAND, COMPLETED, HELP, NAME, FILEPATH
 }
--- src/processor/ErrorProcessor.java
+++ src/processor/ErrorProcessor.java
@@ -1,14 +1,28 @@
+//@@author A0125369Y
 package processor;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
+/**
+ * ErrorProcessor is a class to generate error messages in GUI to alert the
+ * user.
+ * 
+ * @author Barnabas
+ *
+ */
 public class ErrorProcessor {
 
 	private static final String ERROR_TITLE = "Error";
 	private static final String ERROR_TRIGGERED = "Error Triggered";
 	private static final String ERROR_TRIGGERED_AT = "Error Triggered at ";
 
+	/**
+	 * Method to display an error message.
+	 * 
+	 * @param error
+	 *            the error message in String
+	 */
 	public static void alert(String error) {
 		try {
 			Alert alert = new Alert(AlertType.ERROR);
@@ -22,6 +36,14 @@ public class ErrorProcessor {
 		}
 	}
 
+	/**
+	 * Method to display an error message.
+	 * 
+	 * @param errorLocation
+	 *            the location of the error in String
+	 * @param error
+	 *            the error message in String
+	 */
 	public static void alert(String errorLocation, String error) {
 		try {
 			Alert alert = new Alert(AlertType.ERROR);
